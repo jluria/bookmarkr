53f6fcb5a0424fee230c077dcad80fdfa8eb5d57
Josh Luria <josh.luria@gmail.com>
2014-12-18 12:35:49 -0500

    Add links to details template for lists

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -3,4 +3,5 @@ from bookmarks import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<list_id>\d+)/$', views.detail, name='detail'),
 )
--- bookmarks/views.py
+++ bookmarks/views.py
@@ -3,18 +3,13 @@ from django.http import HttpResponse
 from django.views.generic import View
 from bookmarks.models import List
 
-
 def index(request):
     all_lists = List.objects.all()
 
     return render(request, 'bookmarks/index.html', {'lists': all_lists})
 
-"""
 def detail(request, list_id):
     l = List.objects.get(pk=list_id)
+    all_links = l.links.all()
 
-    return render(request, 'bookmarks/detail.html', {'list': l})
-"""
-# class ListIndex(IndexView):
- #    model = List
-# Create your views here.
+    return render(request, 'bookmarks/detail.html', {'list': l, 'links': all_links})
