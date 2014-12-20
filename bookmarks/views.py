6bdb55d8f2253ea5151bf436ae9eb8da5835ac07
Josh Luria <josh.luria@gmail.com>
2014-12-19 19:30:41 -0500

    Restrict app access to logged in users

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from bookmarks.models import List, Link
 from bookmarks.forms import ListForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     all_lists = List.objects.all()
 
     return render(request, 'bookmarks/index.html', {'lists': all_lists})
 
+@login_required
 def detail(request, list_id):
     l = List.objects.get(pk=list_id)
     all_links = l.links.all()
 
     return render(request, 'bookmarks/detail.html', {'list': l, 'links': all_links})
 
+@login_required
 def create_list(request):
     if request.method == 'POST':
         form = ListForm(request.POST)
